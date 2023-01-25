bf5f3a1184017cec8b579de0df842140660d2dfe
leelo52 <115298517+lelo52@users.noreply.github.com>
2023-01-25 15:15:44 +0900

    Add files via upload

---
+++ AmountModel.java
@@ -0,0 +1,17 @@
+package application;
+
+public class AmountModel {
+	
+	public int asum(int[] ctm){
+		int sumt=0;
+		int m=0;
+		
+		for(int i=0; i<3; i++) {
+			if(i==0) m=1000;  
+			if(i==1) m=2000;  
+			if(i==2) m=3000;  		
+			sumt = sumt + ctm[i] * m;
+		}
+		return sumt;
+	}
+}
